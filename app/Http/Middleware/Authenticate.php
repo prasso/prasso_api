3371625291a75c500dd7d4fb7fd303b613ce9764
bperreault <bcp@faxt.com>
2021-01-18 19:21:36 -0500

    .

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -15,7 +15,6 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            Log::info('redirect to login');
             return route('login');
         }
     }
