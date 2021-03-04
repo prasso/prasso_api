54932cb034371ee46e84a614cf1fa8d853805cbd
bperreault <bcp@faxt.com>
2021-03-04 07:17:16 -0500

    .

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,8 +1,7 @@
 </div>
     <div class="bg-white shadow ">
     <div class="p-14 flex sm:justify-center items-center  ">
-    <a class="pr-1" href="contact">contact</a> | <a href="privacy">privacy</a> | <a href="terms">terms</a>
-    </div>
+     </div>
     <div class="flex sm:justify-center items-center sm:pt-0 ">
     <small>&copy faxt 1999-<?php echo date('Y'); ?></small>
     </div>
