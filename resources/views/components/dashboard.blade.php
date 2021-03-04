39029f0193a0ce4fac13c8a62a961e6c746de223
Prasso App <prassoapp@ip-172-30-0-109.ec2.internal>
2021-03-04 12:29:03 +0000

    .

--- resources/views/components/dashboard.blade.php
+++ resources/views/components/dashboard.blade.php
@@ -5,18 +5,18 @@
     <div class="container px-8 pt-20 pb-4 mx-auto lg:px-4">
         <div class="flex flex-col w-full mb-12 text-left ">
             
-            <h1 class="mb-6 text-2xl font-semibold tracking-tighter text-blue-800 sm:text-6xl title-font">
+            <h1 class="mb-6 text-2xl font-semibold tracking-tighter text-blue-800 sm:text-6xl title-font text-center">
             Mercy Full Farms 
             </h1>
-            <h2 class="mb-1 font-semibold tracking-widest text-blue-600 title-font mb-30">
+            <h2 class="mb-6 font-semibold tracking-widest text-blue-600 title-font ">
             Mercy Full Farms, Inc is a non profit organized and operating exclusively for charitable purposes, specifically to provide emergency and low cost housing to homeless individuals and families, stabilize their situation then help train them to support themselves as productive members of society. We also protect and support special needs foster care children who have aged out of the foster system but do not have family love and support. The Dog House Rescue lawn service and Mercy Cleaning operate alongside of Mercy Full Farms to provide job training and skills to farm residents.
             </h2>
-            <h2 class="mb-1 font-semibold tracking-widest text-blue-600 uppercase title-font justify-center"> Goals</h2>
+            <h2 class="mb-2 font-semibold tracking-widest text-blue-600 uppercase title-font text-center"> Goals</h2>
             <p class="mx-auto text-base font-medium leading-relaxed text-gray-700 lg:w-2/3">
            
 We seek to be a half way house between the streets and individual independence for those who through economic and social circumstances are working toward rehabilitation but are not fully functioning members of society yet.
 </p>
-<h2 class="mb-1 font-semibold tracking-widest text-blue-600 uppercase title-font justify-center"> Solution</h2>
+<h2 class="mb-2 font-semibold tracking-widest text-blue-600 uppercase title-font text-center"> Solution</h2>
             <p class="mx-auto text-base font-medium leading-relaxed text-gray-700 lg:w-2/3">
            
 
@@ -30,7 +30,8 @@ Housing and care for special needs young adults who have aged out of the foster 
 </section>
 <section class="block " id="section">
     <div class="container flex flex-col items-center justify-center px-10 py-2 mx-auto lg:px-48">
-        
+        <img src="{{ asset('images/mercy_full_logo.jpg') }}" class="block h-9 w-auto" />
+
     </div>
 </section>
 </div>  
