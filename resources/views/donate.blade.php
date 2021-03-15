e913b4aa588439a1ea61009d26593288efacb86e
bperreault <bcp@faxt.com>
2021-03-15 10:59:47 -0400

    update paypal image

--- resources/views/donate.blade.php
+++ resources/views/donate.blade.php
@@ -47,17 +47,11 @@ Checks, and
 Donations made 
 Through PayPay to mercyfullfarms@gmail.com
 Item donations are accepted as well and can be picked up at your convenience.<br><br>
-      <form action="https://www.paypal.com/donate" method="post" target="_top">
-<input type="hidden" name="business" value="RC7R8N7M35V8L" />
-<input type="hidden" name="item_name" value="All donations are Tax Deductible. We accept cash, checks, and donations made through Paypal." />
-<input type="hidden" name="currency_code" value="USD" />
-<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button"  class="border-0" />
-<img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
-</form>
+     
 </div>
       </div>
     
-    
+    <img src="/images/mcfPaypalQR.jpg" style="border:0" />
       
      
 </div>
