6c8092456eff0dbe1ace046533358f1b521b4623
bperreault <bcp@faxt.com>
2021-03-31 07:58:11 -0400

    dashboard rework

--- resources/views/components/dashboard.blade.php
+++ resources/views/components/dashboard.blade.php
@@ -1,36 +1,85 @@
 
-    <div class="items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
+  <div class="shadow overflow-hidden sm:rounded-md">
+  <div class="p-12 bg-white col-span-12 items-center ">
+@if ( Auth::user() != null)
 
-<section class="text-gray-700 body-font">
-    <div class="container px-8 pt-20 pb-4 mx-auto lg:px-4">
-        <div class="flex flex-col w-full mb-12 text-left lg:text-center">
-            <h2 class="mb-1 font-semibold tracking-widest text-blue-600 uppercase title-font">
-            Your No-Code solution<br class="md:hidden">
-                to an app for your business.</h2>
-            <h1 class="mb-6 text-2xl font-semibold tracking-tighter text-blue-800 sm:text-6xl title-font">
-            Prasso 
-            </h1>
-            
-            <p class="mx-auto text-base font-medium leading-relaxed text-gray-700 lg:w-2/3">
-            Fast API eXtraction Technologies - faxt - presents Prasso. 
-            Prasso is a simple framework to create ANY app. Useful for fast prototyping too!
-            Use these tools to assemble your personalized mobile app, then add your team to provide access.
-            With Prasso, you designate which tabs to include in a mobile application and who can use it.</p>
-             <p>Prasso setup enables you to set up your mobile app with no code required.</p>
+
+        <div class="max-w-xs p-0 m-auto">
+            <div class="flex px-4">
+                <div class="flex-shrink-0">
+                    <img class="h-10 w-10 rounded-full" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
+                </div>
+
+                <div class="ml-3">
+                    <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
+                    <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
+                </div>
+            </div>
+
+            <div class="mt-3 space-y-1">
+                <!-- Account Management -->
+                <x-jet-responsive-nav-link href="{{ route('profile.show') }}" :active="request()->routeIs('profile.show')">
+                    {{ __('Profile') }}
+                </x-jet-responsive-nav-link>
+                <div class="border-t border-gray-200 mt-2"></div>
+
+                <div class="block px-4 py-2 text-xs text-gray-400">
+                    {{ __('Manage Apps') }}
+                </div>
+                <x-jet-responsive-nav-link href="{{ route('apps.show', Auth::user()->allTeams()->first()->id)  }}">
+                    {{ __('Apps') }}
+                </x-jet-responsive-nav-link>
+
+                <x-jet-responsive-nav-link href="{{ route('sites.show', Auth::user()->allTeams()->first()->id)  }}">
+                    {{ __('Sites') }}
+                </x-jet-dropdown-link>
+
+                <!-- Team Management -->
+                @if (Laravel\Jetstream\Jetstream::hasTeamFeatures())
+                    <div class="border-t border-gray-200 mt-2"></div>
+
+                    <div class="block px-4 py-2 text-xs text-gray-400">
+                        {{ __('Manage Team') }}
+                    </div>
+
+                    <!-- Team Settings -->
+                    <x-jet-responsive-nav-link href="{{ route('teams.show', Auth::user()->currentTeam->id) }}" :active="request()->routeIs('teams.show')">
+                        {{ __('Team Settings') }}
+                    </x-jet-responsive-nav-link>
+
+                    <x-jet-responsive-nav-link href="{{ route('teams.create') }}" :active="request()->routeIs('teams.create')">
+                        {{ __('Create New Team') }}
+                    </x-jet-responsive-nav-link>
+
+                    <div class="border-t border-gray-200"></div>
+
+
+                @if (count(Auth::user()->allTeams()) > 1)
+                    <!-- Team Switcher -->
+                    <div class="block px-4 py-2 text-xs text-gray-400">
+                        {{ __('Switch Teams') }}
+                    </div>
+
+                    @foreach (Auth::user()->allTeams() as $team)
+                        <x-jet-switchable-team :team="$team" component="jet-responsive-nav-link" />
+                    @endforeach
+                    @endif
+                @endif
+
+
+                <!-- Authentication -->
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+
+                    <x-jet-responsive-nav-link href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                        {{ __('Logout') }}
+                    </x-jet-responsive-nav-link>
+                </form>
+
+            </div>
         </div>
-        @if (auth()->user() == null)
-        <div class="flex lg:justify-center">
-            <a href="/login"
-                class="px-8 py-2 font-semibold text-white transition duration-500 ease-in-out transform rounded-lg shadow-xl bg-gradient-to-r from-blue-700 hover:from-blue-600 to-blue-600 hover:to-blue-700 focus:ring focus:outline-none">Sign In</a>
-        </div>
-        @endif
-    </div>
-</section>
-<section class="block " id="section">
-    <div class="container flex flex-col items-center justify-center px-10 py-2 mx-auto lg:px-48">
-        <img class="object-cover object-center mx-auto mb-10 rounded-lg lg:w-full md:w-14/5 w-18/6" alt="hero"
-            src="/images/butterfly.png">
-    </div>
-</section>
-</div>  
-  
+ 
+@endif
+
