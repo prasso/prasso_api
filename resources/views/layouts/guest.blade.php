2e9e82d2ba7a9567fe1492c07b2746bde0e5e27b
Bobbi Perreault <bcp@faxt.com>
2021-04-24 08:49:10 -0400

    Update guest.blade.php

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -7,26 +7,6 @@
 
         <title>{{ config('app.name', 'Prasso') }} - {{ $title }}</title>
 
-        <script src="https://tilda.cc/js/jquery-1.10.2.min.js"  type="text/javascript"></script>
-
-<script src="https://static.tildacdn.com/js/tilda-scripts-3.0.min.js"  type="text/javascript"></script>
-
-<link href="https://tilda.cc/css/tilda-grid-3.0.min.css" rel="stylesheet" media="screen">
-
-<link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&subset=latin,cyrillic" rel="stylesheet">		
-
-<link href="https://tilda.cc/projects/style/?projectid=4017550" rel="stylesheet" media="screen">
-<script src="https://tilda.cc/projects/js/?projectid=4017550"></script>
-
-
-        <script src="https://static.tildacdn.com/js/tilda-animation-ext-1.0.min.js"></script>
-    <script src="https://static.tildacdn.com/js/tilda-animation-sbs-1.0.min.js"></script>
-
-<script type="text/javascript">
-    window.ver='67245';
-</script>
-
 <script type="text/javascript"
 src="https://prasso.outseta.com/Scripts/client/dist/outseta.nocode.widget.min.js">
 </script>
