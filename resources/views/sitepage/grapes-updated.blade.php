300eab4bcc1b34f8d946aa4fcb6b6e90412cb56e
Bobbi <bcp@faxt.com>
2023-05-09 09:08:14 -0400

    add editable access to code and fix link on fbc master

--- resources/views/sitepage/grapes-updated.blade.php
+++ resources/views/sitepage/grapes-updated.blade.php
@@ -498,6 +498,69 @@
         }
       });
 
+      // editor panel not readonly
+      var pfx = editor.getConfig().stylePrefix;
+      var codeViewer = editor.CodeManager.getViewer('CodeMirror').clone();
+      var container = document.createElement('div');
+      var btnEdit = document.createElement('button');
+
+      codeViewer.set({
+          codeName: 'htmlmixed',
+          readOnly: 0,
+          theme: 'hopscotch',
+          autoBeautify: true,
+          autoCloseTags: true,
+          autoCloseBrackets: true,
+          lineWrapping: true,
+          styleActiveLine: true,
+          smartIndent: true,
+          indentWithTabs: true
+      });
+
+      btnEdit.innerHTML = 'Edit';
+      btnEdit.className = pfx + 'btn-prim ' + pfx + 'btn-import';
+      btnEdit.onclick = function() {
+          var code = codeViewer.editor.getValue();
+          editor.DomComponents.getWrapper().set('content', '');
+          editor.setComponents(code.trim());
+          modal.close();
+      };
+
+      cmdm.add('html-edit', {
+          run: function(editor, sender) {
+              sender && sender.set('active', 0);
+              var viewer = codeViewer.editor;
+              modal.setTitle('Edit code');
+              if (!viewer) {
+                  var txtarea = document.createElement('textarea');
+                  container.appendChild(txtarea);
+                  container.appendChild(btnEdit);
+                  codeViewer.init(txtarea);
+                  viewer = codeViewer.editor;
+              }
+              var InnerHtml = editor.getHtml();
+              var Css = editor.getCss();
+              modal.setContent('');
+              modal.setContent(container);
+              codeViewer.setContent(InnerHtml + "<style>" + Css + '</style>');
+              modal.open();
+              viewer.refresh();
+          }
+      });
+
+      pn.addButton('options',
+          [
+              {
+                  id: 'edit',
+                  className: 'fa fa-edit',
+                  command: 'html-edit',
+                  attributes: {
+                      title: 'Edit'
+                  }
+              }
+          ]
+      );
+
       // Add info command
       var mdlClass = 'gjs-mdl-dialog-sm';
       var infoContainer = document.getElementById('info-panel');
