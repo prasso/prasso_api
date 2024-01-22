dea2c4685953d830912daa915f9baecd383454bb
Bobbi <bcp@faxt.com>
2024-01-22 11:06:40 -0500

    customize 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,11 +1,169 @@
+
 <!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="UTF-8">
-    <title>Page Not Found - 404 Error</title>
-  </head>
-  <body>
-    <h1>Page Not Found</h1>
-    <p>We're sorry, the page you are looking for cannot be found. Please check the URL and try again.</p>
-  </body>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="YkdRAWPjfw0mXUSeMZ7O0pIxFp77P429S6JDb59G">
+
+        <title>Print Shop - ERP</title>
+
+        <meta name="description" content="" />
+        <meta property="og:title" content="Print Shop - " />
+        <meta property="og:description" content="" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="/" />
+        <meta property="og:image" content="https://images.prasso.io/prasso/user-files/109/colorpluslogo.jpg" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Print Shop - " />
+        <meta name="twitter:description" content="" />
+        <meta name="twitter:image" content="https://images.prasso.io/prasso/user-files/109/colorpluslogo.jpg" />
+
+        <link rel="icon" type="image/png" href="favicon.ico" />
+        <link rel="icon" type="image/png" href="https://images.prasso.io/printshop/favicon-16x16.png" sizes="16x16">
+        <link rel="icon" type="image/png" href="https://images.prasso.io/printshop/favicon-32x32.png" sizes="32x32">
+        <link rel="icon" sizes="192x192" href="https://images.prasso.io/printshop/android-chrome-192x192.png">
+        <link rel="icon" sizes="512x512" href="https://images.prasso.io/printshop/android-chrome-512x512.png">
+        <link rel="apple-touch-icon" href="https://images.prasso.io/printshop/apple-touch-icon.png">
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+                <script src="/js/jquery1.10.0.min.js"></script> 
+                        <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.2.1/dist/alpine.js" defer></script>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap">
+                        <link rel="stylesheet" href="/css/app.css"> 
+                        <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+                                <link href="/js/google-fonts-inter.css" rel="stylesheet">
+                        <link href="/js/google-fonts-material-icon.css" rel="stylesheet">
+                        <link rel="stylesheet" href="/js/google-material-design-iconic-font.2.2.0.min.css">
+                        <link href="/js/google-fonts-Roboto.css" rel="stylesheet"> 
+                        <script src="/js/jqueryui.1.12.1.min.css"></script>
+        
+                    <script></script>
+        
+     
+        <link rel="stylesheet" href="https://printshop.prasso.io/css/app.css">
+                    <style>.teambutton {color:#ffffff;background-color: #f57c1f;}</style>
+                            <style></style>
+        
+    </head>
+    <body>
+                <div class="font-sans text-gray-900 antialiased">
+        <style>
+  body {
+  font-family: 'Open Sans', sans-serif;
+  color: #111;
+  background-color: #ffffff;
+  line-height: 1.5;
+  margin:20px;
+  }
+    .testimonial {
+  border: 1px solid #ccc;
+  padding: 20px;
+  margin: 20px 0;
+}
+
+.testimonial p:first-of-type {
+  font-style: italic;
+}
+
+.testimonial p:last-of-type {
+  font-size: 0.8em;
+  color: #111;
+}
+header {  align-items: center;}
+
+header img {
+  float: left;
+  position: relative;
+  left:50px;
+  top: -120px;
+  margin: 20px;
+}
+
+header h1 {
+  font-size: 2em;
+}
+.hero {
+  background-color: #f57c1f;
+  color: #fff;
+  padding: 150px 0;
+  text-align: center;
+}
+.hero h1 {
+  font-size: 48px;
+  margin: 0;
+}
+.hero p {
+  font-size: 24px;
+  margin: 0;
+  text-align: center;
+  color:#964817;
+  max-width: 80%;
+}
+p {
+  font-size: 18px;
+  padding: 2%;
+  text-align:center;
+
+}
+nav ul li {float:right; padding:10px;font-weight: 900;}
+nav {height: 50px;margin:0; padding:3px; background: #ffffff;}
+section {clear:both; border-bottom: solid 1px #f57c1f; padding: 20px; margin: 20px 0;}
+section h2 {color:#964817;font-size: 36px; text-align:center; margin: 0; padding: 5px;}
+section ul{text-align:center;}
+section table{text-align:center;  margin-left: auto;
+  margin-right: auto;}
+#about {background-color: #fff;}
+#features {background-color: #fff;}
+#pricing {background-color: #fff;}
+#testimonials {background-color: #fff;}
+#signup {background-color: #fff;}
+</style>
+      
+      <div class="hero">
+         <header>
+            <img src="https://images.prasso.io/prasso/user-files/109/colorpluslogo.jpg" alt="Print Shop">      
+            <h1>Print Shop</h1>
+         </header>
+
+         <p>Revolutionary print shop management software designed to optimize every aspect of a print shop, ensures a smooth and organized printing process.</p>
+      </div>
+      <main>
+         <section id="about">
+            <h2>About Our Service</h2>
+            <p>Revolutionize your print shop operations with our cutting-edge management software. Streamline workflows, track orders effortlessly, manage inventory seamlessly, and enhance overall efficiency. From order creation to delivery, our software is designed to optimize every aspect of your print shop business, ensuring a smooth and organized printing process.</p>
+         </section>
+         <section id="features">
+            <h2>Key Features</h2>
+            <ul>
+  <li><strong>Efficient Order Management:</strong> Streamline and organize orders seamlessly.</li>
+  <li><strong>Workflow Optimization:</strong> Enhance printing processes from creation to delivery.</li>
+  <li><strong>Inventory Management:</strong> Easily track and manage your print shop inventory.</li>
+  <li><strong>Customization Options:</strong> Tailor orders to meet the unique needs of your clients.</li>
+  <li><strong>Real-time Tracking:</strong> Keep clients informed with live order status updates.</li>
+  <li><strong>User-friendly Interface:</strong> Intuitive design for easy navigation and usage.</li>
+  <li><strong>Reporting and Analytics:</strong> Gain insights into your print shop performance with detailed reports.</li>
+  <li><strong>Secure Data Handling:</strong> Ensure the confidentiality and security of customer and business data.</li>
+  <li><strong>Print Shop Efficiency:</strong> Maximize overall operational efficiency and productivity.</li>
+</ul>
+
+         </section>
+   
+      </main>
+      <footer>
+         <p>Copyright 2024</p>
+      </footer>
+
+        </div>
+        </div>
+<div class="bt-1 bg-white shadow text-gray-500">
+    <div class="p-14 flex sm:justify-center items-center  ">
+       <a href="https://prasso.io/privacy">privacy</a> | <a href="https://prasso.io/terms">terms</a>
+    </div>
+    <div class="flex sm:justify-center items-center sm:pt-0 ">
+        <small>&copy faxt 1999-2024 and DeductionTraq</small>
+    </div>
+</div>
+    </body>
 </html>
