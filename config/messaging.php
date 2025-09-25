58ac737d17b924dd045bb976fc9fd53c613096ba
bperreault <bcp@faxt.com>
2025-09-25 16:44:05 -0400

    update environment to support sending messages to members

--- config/messaging.php
+++ config/messaging.php
@@ -9,6 +9,9 @@ return [
       
     ],
     'user_model' => App\Models\User::class,
+    // Optional: Fully qualified class name of your domain Member model.
+    // Preferably implements Prasso\Messaging\Contracts\MemberContact.
+    'member_model' => env('MESSAGING_MEMBER_MODEL'),
     'date_format' => env('MESSAGING_DATE_FORMAT', config('app.date_format', 'd/m/Y')),
 
 ];
